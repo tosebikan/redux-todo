1e97c37dd3ef01c8e4a15888a1835c25811ae23d
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-05-21 09:34:53 +0000

    delete function complete

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ class App extends Component {
   state = {
     todo: ['todo 1', 'todo 2', 'todo 3']
   }
+
+  handleDelete = (index) => {
+    const newArr = [...this.state.todo]
+    newArr.splice(index, 1)
+    this.setState({todo: newArr})
+  }
+
   render(){
   return (
     <div className="App">
       <div className="Card frame">
         <Header todoLength={this.state.todo.length}/>
-        <TodoList task={this.state.todo}/>
+        <TodoList task={this.state.todo} onDelete={this.handleDelete}/>
         <h3>submit</h3>
       </div>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './TodoList.css';
 
-const Todo = ({content}) =>{
+const Todo = ({content, onDelete, id}) =>{
   return(
     <div className="List">
       {content}
-      <button className="Toggle"> </button>
+      <button className="Toggle"
+      onClick={()=> {onDelete(id)}}> </button>
     </div>
   )
 }
 
-const TodoList = ({task}) => {
+const TodoList = ({task, onDelete}) => {
   const todos = task.map((todo, index) => {
-    return <Todo content={todo} key={index} id={index} />
+    return <Todo content={todo} key={index} id={index} onDelete={onDelete}/>
   })
   return (
     <div className="TodoList">
